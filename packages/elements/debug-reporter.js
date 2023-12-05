90c764773071666835094a387a43aee43d8de529
Hugo van Rijswijk <git@hugovr.nl>
2023-12-05 16:45:55 +0100

    formatting

--- packages/elements/debug-reporter.js
+++ packages/elements/debug-reporter.js
@@ -2,8 +2,10 @@
 import { declareValuePlugin, PluginKind } from '@stryker-mutator/api/plugin';
 import fs from 'fs';
 
-export const strykerPlugins = [declareValuePlugin(PluginKind.Reporter, 'debug', {
-  onDryRunCompleted({ result }) {
-    fs.writeFileSync('./mutation-coverage.json', JSON.stringify(result.mutantCoverage, null, 2));
-  },
-})];
+export const strykerPlugins = [
+  declareValuePlugin(PluginKind.Reporter, 'debug', {
+    onDryRunCompleted({ result }) {
+      fs.writeFileSync('./mutation-coverage.json', JSON.stringify(result.mutantCoverage, null, 2));
+    },
+  }),
+];
